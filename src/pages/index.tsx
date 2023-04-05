384d70813d6d07b78f7378ad0540892172d398c1
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2023-04-06 00:38:02 +0300

    feat: Adds the navigation bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,31 @@
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "../components";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
+  );
 }
