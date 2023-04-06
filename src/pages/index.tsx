82847acf94b5828186dd85e027a60cc2db82c37a
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2023-04-06 16:46:09 +0300

    fix: imports Computers dynamically to the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,24 @@
+import dynamic from "next/dynamic";
 import {
   About,
   Contact,
   Experience,
   Feedbacks,
-  Hero,
   Navbar,
   Tech,
   Works,
   StarsCanvas,
 } from "../components";
 
+const Computers = dynamic(() => import("@/components/canvas/Computers"), {
+  ssr: false,
+});
+
 export default function Home() {
   return (
     <div className="relative z-0 bg-primary">
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
-        <Hero />
       </div>
       <About />
       <Experience />
@@ -29,3 +32,5 @@ export default function Home() {
     </div>
   );
 }
+
+Home.canvas = () => <Computers />;
