8dfa37127ffbde161db21440fcb8f768493e045c
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2023-04-06 17:25:36 +0300

    feat: wraps the Hero component with `<Canvas />``

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,32 +3,35 @@ import { motion } from "framer-motion";
 import { styles } from "../styles/styles";
 // import { ComputersCanvas } from "./canvas";
 import { Suspense, lazy } from "react";
+import { Canvas } from "@react-three/fiber";
 
 const ComputersComponent = lazy(() => import("./canvas/Computers"));
 
 const Hero = () => {
   return (
     <Suspense fallback="loading">
-      <section className="relative w-full h-screen mx-auto">
-        <div
-          className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-auto items-start gap-5`}
-        >
-          <div className="flex flex-col justify-center items-center mt-5">
-            <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-            <div className="w-1 sm:h:80 h-40 violet-gradient" />
+      <Canvas>
+        <section className="relative w-full h-screen mx-auto">
+          <div
+            className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-auto items-start gap-5`}
+          >
+            <div className="flex flex-col justify-center items-center mt-5">
+              <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+              <div className="w-1 sm:h:80 h-40 violet-gradient" />
+            </div>
+            <div>
+              <h1 className={`${styles.heroHeadText} text-white`}>
+                Hi, I&apos;m <span className="text-[#915eff]">Itay</span>
+              </h1>
+              <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                I develop user <br className="sm:block hidden" /> interfaces and
+                web applications
+              </p>
+            </div>
           </div>
-          <div>
-            <h1 className={`${styles.heroHeadText} text-white`}>
-              Hi, I&apos;m <span className="text-[#915eff]">Itay</span>
-            </h1>
-            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              I develop user <br className="sm:block hidden" /> interfaces and
-              web applications
-            </p>
-          </div>
-        </div>
-        <ComputersComponent />
-      </section>
+          <ComputersComponent />
+        </section>
+      </Canvas>
     </Suspense>
   );
 };
