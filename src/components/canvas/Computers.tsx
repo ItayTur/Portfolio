61978f69d52bf32e5bbbb47d048e14e393d689d3
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2023-04-07 22:23:23 +0300

    feat: displays the desktop pc 3D image

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Canvas } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useLoader } from "@react-three/fiber";
 
-const Computers = () => {
+const Computers = ({ isMobile = false }) => {
+  const gltf = useLoader(GLTFLoader, "/desktop_pc/scene.gltf");
+
   return (
-    <div>Computers</div>
-  )
-}
+    <group>
+      <primitive
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+        object={gltf.scene}
+      />
+    </group>
+  );
+};
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas>
+      <Computers />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
