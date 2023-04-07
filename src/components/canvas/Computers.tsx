e0b114c006491286300a968074cf1759f5781a4a
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2023-04-07 23:11:03 +0300

    feat: styles the GLTF model

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -1,26 +1,49 @@
-import { Canvas } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { useLoader } from "@react-three/fiber";
+import { useLoader, Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { OrbitControls, Preload } from "@react-three/drei";
 
 const Computers = ({ isMobile = false }) => {
   const gltf = useLoader(GLTFLoader, "/desktop_pc/scene.gltf");
-
   return (
-    <group>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.6}
+        position={[0, -2.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
         object={gltf.scene}
       />
-    </group>
+    </mesh>
   );
 };
 
 const ComputersCanvas = () => {
   return (
-    <Canvas>
-      <Computers />
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={"loading..."}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+        <Preload all />
+      </Suspense>
     </Canvas>
   );
 };
