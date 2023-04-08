023b5d3e7479f13ed6f972f711e8e5af5cc6edf8
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2023-04-08 03:30:10 +0300

    feat: finishes the About section
    1) animates the About section
    2) adds a common section component
    3) replaces tilt packages

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+import { styles } from "../styles/styles";
+import { services } from "../consts";
+import { fadeIn, textVariant } from "../utils/motion";
+import Section from "./Section";
+
+const ServiceCard = ({
+  title,
+  index,
+  icon,
+}: {
+  title: string;
+  index: number;
+  icon: any;
+}) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div className="bg-tertiay rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+          <Image src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <Section idName="about">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Some text about me
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </Section>
+  );
+};
 
-export default About
+export default About;
