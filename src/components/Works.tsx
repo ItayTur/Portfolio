515072da1810364747e16de5ea884ba34de0c3ae
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2023-04-09 21:00:47 +0300

    feat: finishes the Works section

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import Tilt from "react-parallax-tilt";
+import { Project, Sections, projects, worksDescription } from "@/consts";
+import { styles } from "@/styles/styles";
+import { fadeIn, textVariant } from "@/utils/motion";
+import { motion } from "framer-motion";
+import React from "react";
+import Image from "next/image";
+import { github } from "../../public/assets";
+import Section from "./Section";
+
+const ProjectCard = ({
+  image,
+  name,
+  source_code_link,
+  description,
+  tags,
+}: Project) => {
+  return (
+    <motion.div>
+      <Tilt
+        scale={1}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <Image
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <a
+              href={source_code_link}
+              target="_blank"
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <Image
+                src={github}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </a>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-xl">{name}</h3>
+          <p className="mt-2 text-secondary text-sm">{description}</p>
+        </div>
+        <div className="flex flex-wrap gap-2">
+          {tags.map(({ name, color }) => (
+            <p key={name} className={`text-sm ${color}`}>
+              #{name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
 
 const Works = () => {
   return (
-    <div>Works</div>
-  )
-}
+    <Section idName={Sections.Works}>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-lg max-w-3xl leading-7"
+        >
+          {worksDescription}
+        </motion.p>
+      </div>
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project) => (
+          <ProjectCard key={project.name} {...project} />
+        ))}
+      </div>
+    </Section>
+  );
+};
 
-export default Works
+export default Works;
--- src/consts/index.ts
+++ src/consts/index.ts
@@ -203,6 +203,19 @@ export const testimonials = [
   },
 ];
 
+export type Tag = {
+  name: string;
+  color: string;
+};
+
+export type Project = {
+  name: string;
+  description: string;
+  tags: Tag[];
+  image: StaticImageData;
+  source_code_link: string;
+};
+
 export const projects = [
   {
     name: "Car Rent",
@@ -269,11 +282,15 @@ export const projects = [
   },
 ];
 
-export const description =
+export const aboutDescription =
   "As a web developer, I specialize in creating visually stunning and highly functional websites using programming tools such as React, Graphql, Nextjs, Vercel, and more. My goal is to provide an intuitive, engaging user experience that is easy to navigate. With strong project management and communication skills, I work closely with clients and product managers to understand their needs and deliver projects on time and to their satisfaction. I am committed to continuously learning and evolving to provide the best possible service.";
+
+export const worksDescription =
+  "Here you will find a showcase of my practical skills and experience in various web development areas, ranging from frontend and backend development to full-stack web development. Each project entry provides a brief overview of its purpose with links to code repositories and live demos in it. My projects highlight my ability to undertake complex web development tasks and deliver successful outcomes.";
 
 export enum Sections {
   About = "ABOUT",
   Experience = "EXPERIENCE",
   Tech = "TECH",
+  Works = "WORKS",
 }
